c324e37800616c0b4fe877f288741b02973c3a04
Amitbalter <amitbaltern@gmail.com>
2025-01-30 13:44:55 +0000

    task 10

--- app/model.js
+++ app/model.js
@@ -119,3 +119,9 @@ exports.selectCommentById = (comment_id) => {
 exports.removeCommentById = (comment_id) => {
     return db.query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id]);
 };
+
+exports.selectUsers = () => {
+    return db.query("SELECT * FROM users;").then((users) => {
+        return users.rows;
+    });
+};
